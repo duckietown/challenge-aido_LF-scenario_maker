71360caf5c30fe4078c3d7d6aaaedd1894c22a07
Andrea Censi <acensi@ethz.ch>
2020-11-29 15:24:40 +0100

    Bump version: 6.0.22 → 6.0.23

--- scenario_maker.py
+++ scenario_maker.py
@@ -22,7 +22,7 @@ from duckietown_world.world_duckietown.map_loading import _get_map_yaml
 from duckietown_world.world_duckietown.sampling import make_scenario
 
 logger = ZLogger("scenario_maker")
-__version__ = "6.0.22"
+__version__ = "6.0.23"
 logger.info(f"{__version__}")
 
 
